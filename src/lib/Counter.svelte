82edd41dcc95c02140f527f0c9f7a9e85dc738df
AJ <ancheetah@users.noreply.github.com>
2022-10-02 17:11:20 -0400

    reactivity - declarations

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,5 +1,6 @@
 <script>
 	let count = 0;
+  $: doubled = count * 2  // Re-run this code whenever any of the referenced values change
 
 	function incrementCount() { // Event handler
 		count += 1;
@@ -9,3 +10,6 @@
 <button on:click={incrementCount}>  <!-- Bind click to event handler -->
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
+<p>
+  {count} doubled is {doubled}
+</p>
