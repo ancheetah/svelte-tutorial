09794b0e02a790b9a1659ec239f5a6449a2d3f05
AJ <ancheetah@users.noreply.github.com>
2022-10-02 17:17:08 -0400

    reactivity - statements

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,6 +1,15 @@
 <script>
 	let count = 0;
-  $: doubled = count * 2  // Re-run this code whenever any of the referenced values change
+  $: doubled = count * 2  // Declare a reactive value
+                          // Re-run this code whenever any of the 
+                          // referenced values change
+
+  $: if (count >= 10) { // Declare reactive statements
+		alert('count is dangerously high!');
+		count = 9;
+		console.log('the count is ' + count); // Can also declare statements 
+                                          // outside of "if" with {}
+	}
 
 	function incrementCount() { // Event handler
 		count += 1;
