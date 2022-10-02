310718d929d4515b3dd89a081034a03ccf1f155b
AJ <ancheetah@users.noreply.github.com>
2022-10-02 17:03:20 -0400

    reactivity - assignments

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,11 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
-  }
+	let count = 0;
+
+	function incrementCount() { // Event handler
+		count += 1;
+	}
 </script>
 
-<button on:click={increment}>
-  count is {count}
+<button on:click={incrementCount}>  <!-- Bind click to event handler -->
+	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
